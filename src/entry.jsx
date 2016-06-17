c131efd0d061282153bffbe1360af250dbc2ef4a
wayne1986 <huhuanfengwayne@163.com>
2016-06-17 14:09:06 +1200

    First jekyll site

--- src/entry.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Showcase from './components/Showcase.jsx';
-import IndexPage from './components/IndexPage.jsx';
-import ReactDom from 'react-dom';
-
-require('./css/bootstrap.css');
-
-ReactDom.render(<IndexPage />, document.getElementById('content'));
